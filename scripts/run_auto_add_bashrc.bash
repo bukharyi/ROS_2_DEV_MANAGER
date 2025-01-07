6d1fd31c673786c18dee01399f0f7e90b3e36a5d
Mahir Sehmi <mnmahir@gmail.com>
2025-01-07 10:13:41 +0800

    Fixed auto add alias to bashrc script and improved it

--- scripts/run_auto_add_bashrc.bash
+++ scripts/run_auto_add_bashrc.bash
@@ -15,18 +15,19 @@ fi
 
 BASHRC_LIST_PATH="${1:-$WS_PROJECT_REPO/config/bashrc_list.bash}"
 
+auto_confirm=false
+if [[ "$2" == "-y" ]]; then
+    auto_confirm=true
+fi
+
+source $BASHRC_LIST_PATH
+
 if [ ${#bashrc_list[@]} -eq 0 ]; then
     echo -e "$BASH_ERROR No aliases found in $BASHRC_LIST_PATH"
 else
-    source $BASHRC_LIST_PATH
-
     # Replace spaces in $DEV_PROJET_NAME with underscores and capitalize the string
     DEV_PROJECT_NAME_VAR=$(echo $DEV_PROJECT_NAME | tr ' ' '_' | tr '[:lower:]' '[:upper:]')
 
-    # echo each line in the bashrc_list
-    echo -e "$BASH_INFO Project Name: \e[36m$DEV_PROJECT_NAME\e[0m"
-    # echo -e "$BASH_INFO Project Dev Path: $PROJECT_DEV_PATH"
-    # echo -e "$BASH_INFO Project Path Variable: $DEV_PROJECT_NAME_VAR"
     echo -e "$BASH_INFO The following lines will be added to the .bashrc file:"
     echo -e "=================================================\e[36m"
     for i in "${bashrc_list[@]}"
@@ -35,23 +36,35 @@ else
     done
     echo -e "\e[0m================================================="
 
+    if [ "$auto_confirm" = false ]; then
+        echo -e "$BASH_ACTION  Do you want to add the above aliases to the .bashrc file? [y/n]"
+        read -r response
+    else
+        response="y"
+    fi
 
-    for i in "${bashrc_list[@]}"
-    do
-        # Check if the entire line is in the .bashrc file
-        if ! grep -qF "$i" ~/.bashrc; then
-            echo "$i" >> ~/.bashrc
-            # check if the line is added
-            if grep -qF "$i" ~/.bashrc; then
-                echo -e "$BASH_SUCCESS Added to .bashrc: \e[36m$i\e[0m"
+    if [[ ! $response =~ ^([nN][oO]|[nN])$ ]]
+    then
+        for i in "${bashrc_list[@]}"
+        do
+            # Check if the entire line is in the .bashrc file
+            if ! grep -qF "$i" ~/.bashrc; then
+                echo "$i" >> ~/.bashrc
+                # check if the line is added
+                if grep -qF "$i" ~/.bashrc; then
+                    echo -e "$BASH_SUCCESS Added to .bashrc: \e[36m$i\e[0m"
+                else
+                    echo -e "$BASH_ERROR Could not add to .bashrc: \e[36m$i\e[0m"
+                fi
             else
-                echo -e "$BASH_ERROR Could not add to .bashrc: \e[36m$i\e[0m"
+                echo -e "$BASH_WARNING Already in .bashrc: \e[36m$i\e[0m"
             fi
-        else
-            echo -e "$BASH_WARNING Already in .bashrc: \e[36m$i\e[0m"
-        fi
-    done
-    echo -e "\e[0m================================================="
+        done
+        echo -e "\e[0m================================================="
+    else
+        echo -e "$BASH_INFO Cancelled."
+        echo -e "\e[0m================================================="
+    fi
 
     # # Check if aliases exist in the .bashrc file
     # for i in "${bashrc_list[@]}"
